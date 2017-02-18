784209dc67f6cf85ca5fb693954dbef2c6d338a7
Mateo Juvera <mjm973@nyu.edu>
2017-02-18 14:16:07 -0500

    Added block/floor

---
+++ OctocatAdventure/Assets/Scripts/SnapToGrid.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnapToGrid : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        snap(transform);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    static void snap(Transform t) {
+        float x = (Mathf.Floor(t.position.x) * 2f + 1) * 0.5f;
+        float y = (Mathf.Floor(t.position.y) * 2f + 1) * 0.5f;
+
+        t.position = new Vector3(x, y);
+    }
+}
