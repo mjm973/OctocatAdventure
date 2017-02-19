b6b137a22d0c9dd89606509e3d0280ee60e4923e
Mateo Juvera <mjm973@nyu.edu>
2017-02-18 23:07:36 -0500

    SFX, enemy FX

--- OctocatAdventure/Assets/Scripts/CameraTrack.cs
+++ OctocatAdventure/Assets/Scripts/CameraTrack.cs
@@ -10,22 +10,53 @@ public class CameraTrack : MonoBehaviour {
     float prevX = 0, prevY = 0; // Camera's previous position
     float harshness = 0.2f;
 
-	// Use this for initialization
-	void Start () {
+    // SCREEN SHAKE
+    float time = 10;
+    float shakeAmount = 5;
+    float shakePeriod = 0, amplitude = 0;
+    float randomSeed = 0;
+    float camZ;
+
+    // Use this for initialization
+    void Start() {
         octo = GameObject.Find("Octocat");
         gm = GameObject.Find("GameMaster").GetComponent<gmScript>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
+        camZ = transform.position.z; // Save camera depth
+    }
+
+    // Update is called once per frame
+    void Update() {
         harshness = gm.camHarshness;
 
         newX = Mathf.Lerp(prevX, octo.transform.position.x, harshness);
         newY = Mathf.Lerp(prevY, octo.transform.position.y, harshness);
 
-        transform.position = new Vector3(newX, newY, transform.position.z);
+        if (time < shakePeriod) {
+            float angle = time * Mathf.PI / (shakePeriod); // Angle goes from 0 to Pi over the period of shaking
+            amplitude = shakeAmount * Mathf.Sin(angle); // Angle drives a sine function to determine base amount of shaking
 
+            // Using a random seed, obtain values from Perlin Noise for a smoother, more unpredictable shake
+            float perlinX = (Mathf.PerlinNoise(randomSeed + time * 10, randomSeed) - 0.5f) * 2;
+            float perlinY = (Mathf.PerlinNoise(randomSeed, randomSeed + time * 10) - 0.5f) * 2;
+
+            // Set new position (AKA shake)
+            Vector3 newPos = new Vector3(newX + perlinX * amplitude, newY + perlinY * amplitude, camZ);
+            transform.position = newPos;
+
+            time += Time.deltaTime;
+        }
+        else {
+            transform.position = new Vector3(newX, newY, camZ); // Define origin as default camera position
+        }
         prevX = newX;
         prevY = newY;
-	}
+    }
+
+    // When called, sets shaking period, base shake amount, chooses a random seed, and resets the timer
+    void Shake(Vector2 periodAmp) {
+        shakePeriod = periodAmp.x;
+        shakeAmount = periodAmp.y;
+        randomSeed = Random.Range(0f, 100f);
+        time = 0;
+    }
 }
--- OctocatAdventure/Assets/Scripts/EnemyWalk.cs
+++ OctocatAdventure/Assets/Scripts/EnemyWalk.cs
@@ -4,18 +4,25 @@ using UnityEngine;
 
 public class EnemyWalk : MonoBehaviour {
     Rigidbody2D rb;
+    SpriteRenderer sr;
     Animator anim;
     gmScript gm;
+
+    int health = 3;
     int dir = 1;
     bool bRight = false;
+
+    GameObject cam;
 
     RaycastHit2D[] _maybeRaycast;
 
 	// Use this for initialization
 	void Start () {
         rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
         gm = GameObject.Find("GameMaster").GetComponent<gmScript>();
         anim = GetComponent<Animator>();
+        cam = GameObject.Find("Main Camera");
         _maybeRaycast = new RaycastHit2D[10];
     }
 	
@@ -45,5 +52,50 @@ public class EnemyWalk : MonoBehaviour {
         }
 
         return true;
+    }
+
+    void damage(int amount) {
+        health -= amount;
+
+        StartCoroutine("blink");
+
+
+        StartCoroutine("colors");
+
+
+        cam.SendMessage("Shake", new Vector2(0.3f, 1.5f));
+
+        if (health <= 0) {
+            Destroy(gameObject);
+        }
+    }
+    
+    IEnumerator blink() {
+        Color col = sr.color;
+        for (int q = 0; q < 3; ++q) {
+            col.a = 0.2f;
+            sr.color = col;
+
+            yield return new WaitForSeconds(0.1f);
+
+            col.a = 1f;
+            sr.color = col;
+
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+
+    IEnumerator colors() {
+        Color col = sr.color;
+        Color temp = Color.yellow;
+        for (int q = 0; q < 3; ++q) {
+            sr.color = temp;
+
+            yield return new WaitForSeconds(0.1f);
+
+            sr.color = col;
+
+            yield return new WaitForSeconds(0.1f);
+        }
     }
 }
